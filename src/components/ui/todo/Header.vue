67fb1cc947fab34ec53da808a4c01bffadf9f635
Lefterija <lefterija@egoditor.com>
2021-01-26 08:59:36 +0100

    Create header component for todo list Import cnd for the material items

---
+++ src/components/ui/todo/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="app__header">
+    <button class="app__button">
+      <i class="material-icons">sort</i>
+    </button>
+    <h1 class="heading--app">ToDo list</h1>
+    <button class="app__button">
+      <i class="material-icons">add_circle_outline</i>
+    </button>
+  </header>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({})
+export default class UiTodoHeader extends Vue {}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,18 @@
 <template>
   <div class="app__container">
     <div class="app__main">
-      <header class="app__header">
-        <h1 class="heading--app">ToDo list</h1>
-        <button class="app__add-button"></button>
-      </header>
+      <ui-todo-header></ui-todo-header>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-
-@Component({})
+const UiTodoHeader = () => import("@/components/ui/todo/Header.vue");
+@Component({
+  components: {
+    UiTodoHeader
+  }
+})
 export default class Home extends Vue {}
 </script>
