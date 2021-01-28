776e9f52a61177bd4e9bcb4e303616798de1ce74
Lefterija Temelkova <lefterija@egoditor.com>
2021-01-28 10:11:15 +0100

    - Created new component for the todolist item - Created styles for the todolist item

---
+++ src/components/ui/todo/ListItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="todo__item">
+    <div class="todo__item-wrapper">
+      <h6 class="todo__item-title todo__item-title--checked">
+        test
+      </h6>
+      <div class="todo__item-date">
+          22.01.2021
+      </div>
+    </div>
+    <div class="todo__item-checkbox">
+      <input type="checkbox">
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({})
+export default class UiTodoListItem extends Vue {}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="app__container">
     <div class="app__main">
       <ui-todo-header></ui-todo-header>
+      <ui-todo-list-item v-for="n in 10" :key="n"></ui-todo-list-item>
     </div>
   </div>
 </template>
@@ -9,9 +10,11 @@
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 const UiTodoHeader = () => import("@/components/ui/todo/Header.vue");
+const UiTodoListItem = () => import("@/components/ui/todo/ListItem.vue")
 @Component({
   components: {
-    UiTodoHeader
+    UiTodoHeader,
+    UiTodoListItem
   }
 })
 export default class Home extends Vue {}
