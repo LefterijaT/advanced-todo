3ddac66a5cc26825f5091a23f78959b6e364f001
Nemanja Dragun <nemanja@egoditor.com>
2021-02-17 09:53:49 +0100

    feat: Make progress based on how many tasks are completed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,15 @@
   <div class="app__container">
     <div class="app__main">
       <ui-todo-header></ui-todo-header>
+      <div>
+        <progress
+          style="width: 100%"
+          id="file"
+          :value="taskCompletedPercentage"
+          max="100"
+          >{{ taskCompletedPercentage }}%</progress
+        >
+      </div>
       <ui-todo-list-item
         v-for="item in $store.state.todo.list"
         :key="item.id"
@@ -18,11 +27,20 @@
 import { Component, Vue } from "vue-property-decorator";
 const UiTodoHeader = () => import("@/components/ui/todo/Header.vue");
 const UiTodoListItem = () => import("@/components/ui/todo/ListItem.vue");
+import { TodoItem } from "@/interface/TodoItemInterface";
 @Component({
   components: {
     UiTodoHeader,
     UiTodoListItem
   }
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  get taskCompletedPercentage() {
+    const list = this.$store.state.todo.list;
+    const completedTasks = list.filter((task: TodoItem) => task.completed)
+      .length;
+    const totalTasks = list.length;
+    return (completedTasks / totalTasks) * 100;
+  }
+}
 </script>
